31bfc8660768a8766adbe8f2d7c016c0e180abb2
AbdarrahmaneLiasse <abdarrahmaneliassePR@gmail.com>
2023-06-15 11:19:27 -0700

    hello c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,8 @@
+#!/bin/bash
+/#import <stdio.h>
+int main(void)
+{
+	printf("the size of int is :",sizeof(int));
+	printf("the size of float is :",sizeof(float));   
+	printf("the size of long int is :",sizeof(long int));
+}
