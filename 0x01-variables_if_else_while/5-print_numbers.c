1bc55e4976682d2ca1eb5148be866b1c4b076115
AbdarrahmaneLiasse <abdarrahmaneliassePR@gmail.com>
2023-06-16 10:56:05 -0700

    0x01-variables_if_else_while files

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
